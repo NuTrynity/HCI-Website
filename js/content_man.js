8ac37522dd3be5dff7eaa96d02cb665e4f1972be
NuTrynity <vargasromeo716@gmail.com>
2024-12-17 02:49:43 +0800

    ported scripts and css to their respective files

--- js/content_man.js
+++ js/content_man.js
@@ -1,7 +1,18 @@
-function load_data(file) {
-    return fetch(file);
-}
+const container = document.querySelector(".game-cards");
 
-function sort_category(category) {
-    //#TODO PLEASE MAKE A SORTING ALGORITHM
-}
+fetch("sample_data.json")
+.then((Response) => Response.json())
+.then(data => {
+    
+    let content = data.map(function(item) {
+        return `        <div class="game-card">
+                            <img class="image" src="${item.img}" alt="Action">
+                            <h1 class="name">${item.name}</h1>
+                            <p class="desc">${item.desc}</p>
+                            <small class="category">${item.category}</small>
+                        </div>`;
+    })
+
+    content = content.join("");
+    container.innerHTML = content
+});
